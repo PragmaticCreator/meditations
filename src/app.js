1a42c06beea1e0f49ebb3b1451c7746a9a62c553
Aaron Christopher <christopher@Cratos.aaronchristopher.developer@gmail.com>
2020-11-17 23:12:13 +0530

    make it better again

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const main = () => {
   const PORT = process.env.PORT || 5000;
 
   app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} on PORT:${PORT}`);
+    console.log(`Server running in on PORT:${PORT}`);
   });
 };
 
